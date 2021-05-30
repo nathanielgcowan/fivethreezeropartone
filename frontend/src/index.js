911d4fadcd188c572d74eb0da2d31f45adbf6780
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-05-30 07:29:11 -0500

    made the html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const cREATURES_URL = "http://localhost:3000/creatures"
+// this is where skill will go soon
+const makeACreature = document.querySelector(".makeacreature")
+const enterCreatureName = document.querySelector(".creaturename")
+const enterCreatureImage = document.querySelector(".creatureimage")
+const enterCreatureDescription = document.querySelector(".creaturedescription")
+const deck = document.querySelector(".deck")
+const searchBar = document.querySelector("#searchBar")
+const color = document.querySelector("#color")
